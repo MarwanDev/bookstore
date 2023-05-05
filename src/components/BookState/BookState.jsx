ddb54b3d6a82323845e43c16a7dd0ae76f8b44a7
MarwanDev <marwaneg@aucegypt.edu>
2023-05-06 00:13:21 +0300

    Add props to bookState

--- src/components/BookState/BookState.jsx
+++ src/components/BookState/BookState.jsx
@@ -1,18 +1,27 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import PropTypes from 'prop-types';
 import './BookState.scss';
 
-function BookState() {
+function BookState(props) {
+  const { book } = props;
+  const {
+    id,
+    title,
+    author,
+    genre,
+  } = book;
   return (
     <div className="book-container">
       <div className="book-info-container">
         <h2 className="book-genre">
-          Action
+          {genre}
         </h2>
         <h3 className="book-title">
-          The Hunger Games
+          {title}
         </h3>
         <h4 className="book-author">
-          Suzanne Collins
+          {author}
         </h4>
       </div>
       <ul className="options-container">
@@ -24,4 +33,12 @@ function BookState() {
   );
 }
 
+BookState.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default BookState;
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -2,10 +2,27 @@
 import React from 'react';
 import { BookState, BookForm } from '../../components';
 
+const booksList = [
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    genre: 'Action',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    genre: 'Science Fiction',
+  },
+];
+
 function Books() {
   return (
     <>
-      <BookState />
+      {booksList.map((book) => (
+        <BookState key={book.id} book={book} />
+      ))}
       <BookForm />
     </>
   );
