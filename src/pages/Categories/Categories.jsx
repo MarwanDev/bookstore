04cbbf5a8d5edc315ec78bfe5f7b588e4983425f
MarwanDev <marwaneg@aucegypt.edu>
2023-05-09 14:54:48 +0300

    Add redux components

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Categories() {
+  const status = useSelector((state) => state.category.status);
   return (
-    <div />
+    <h1>{status}</h1>
   );
 }
 
